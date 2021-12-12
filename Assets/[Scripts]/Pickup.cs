602ad4faf7d50b2a1e4e97e77acf11c47ae45797
Amer Ali <ameralizee93@gmail.com>
2021-12-12 11:54:52 -0500

    Updated project settings and added random colored pickups.

--- Assets/[Scripts]/Pickup.cs
+++ Assets/[Scripts]/Pickup.cs
@@ -1,7 +1,7 @@
 // PROGRAM NAME: GAME Programming (T163)
 // STUDENT ID : 101206769
 // AUTHOR     : AMER ALI MOHAMMED
-// CREATE DATE     : Nov 26, 2021
+// CREATE DATE     : Dec 26, 2021
 // PURPOSE     : GAME2014_F2021_ASSIGNMENT2_Part1
 // SPECIAL NOTES:
 // ===============================
@@ -12,21 +12,60 @@
 // Change History:
 // Added score
 //==================================
+// Change History:
+// Added random colored pickups
+//==================================
 
 
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Pickup : MonoBehaviour
 {
+    [Header("Pickup items")]
+    public GameObject yellowStar;
+    public GameObject redStar;
+    public GameObject greenStar;
+    public int num;
+
+
     private Transform playerTransform;
+    
 
     // Start is called before the first frame update
     void Start()
     {
         playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+        num = Random.Range(1,4); // generating a random number between 3 values.
 
+    }
+
+    private void Update()
+    {
+        if (num == 1)
+        {
+            
+            yellowStar.SetActive(true); // Activates yellow
+            redStar.SetActive(false);
+            greenStar.SetActive(false);
+
+
+        }
+        if (num == 2)
+        {
+            redStar.SetActive(true); // Activates red
+            greenStar.SetActive(false);
+            yellowStar.SetActive(false);
+
+        }
+        if (num == 3)
+        {
+            greenStar.SetActive(true); // Activates green
+            redStar.SetActive(false);
+            yellowStar.SetActive(false);
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D other)
